d0aa770784ef18cf1a3c488660a957f7b5179a70
Artem <s02210466@gse.cs.msu.ru>
2024-01-15 02:24:55 +0300

    frontend added

--- bot_site/bot_management/urls.py
+++ bot_site/bot_management/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bot.urls'), name='home'),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout', views.custom_logout, name='logout'),
+    path('accounts/logout/', views.custom_logout, name='logout'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- bot_site/bot_management/views.py
+++ bot_site/bot_management/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import logout
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def custom_logout(request):
     logout(request)
-    return render(request, "registration/logged_out.html")
+    # return render(request, "registration/logged_out.html")
+    return redirect('/')
